d902562667948a1e1dfd24312b45c7b24e25cc56
Aman <amanyadav794172@gmail.com>
2022-08-07 20:53:57 +0530

    first commit

---
+++ project3/index.js
@@ -0,0 +1,96 @@
+console.log("This is my index js file")
+
+// Initialize the news api parameters
+let source = 'the-times-of-india';
+let apikey = 'f6551984c8ae495b86393b5cbe2994fc'
+
+// Grab the news container
+let newsAccordion = document.getElementById('newsAccordion')
+
+// Create a get request
+const xhr = new XMLHttpRequest();
+
+// Open the object
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apikey}`, true);
+
+// What to do when response is ready
+xhr.onload = function() {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText)
+        let articles = json.articles
+        console.log(articles)
+        let newsHtml = "";
+        articles.forEach(function(element, index) {
+            let news = `<div class="card">
+                            <div class="card-header" id="heading${index}">
+                                <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                    aria-expanded="false" aria-controls="collapse${index}">
+                                   <b>Breaking News ${index+1}:</b> ${element["title"]}
+                                </button>
+                                </h2>
+                            </div>
+
+                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
+                                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
+                            </div>
+                        </div>`;
+            newsHtml += news;
+
+        })
+        newsAccordion.innerHTML = newsHtml;
+    } else {
+        console.log("some error occured")
+    }
+}
+
+xhr.send()
+    // ${element["title"]}
+    // ${element["content"]}.
+    // <a href="${element['url']}" target="_blank">Read more here</a>
+
+// console.log("This is my index js file");
+
+// // Initialize the news api parameters
+// let source = 'the-times-of-india';
+// let apiKey = 'f6551984c8ae495b86393b5cbe2994fc'
+
+// // Grab the news container
+// let newsAccordion = document.getElementById('newsAccordion');
+
+// // Create an ajax get request
+// const xhr = new XMLHttpRequest();
+// xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`, true);
+
+// // What to do when response is ready
+// xhr.onload = function() {
+//     if (this.status === 200) {
+//         let json = JSON.parse(this.responseText);
+//         let articles = json.articles;
+//         console.log(articles);
+//         let newsHtml = "";
+//         articles.forEach(function(element, index) {
+//             // console.log(element, index)
+//             let news = `<div class="card">
+//                             <div class="card-header" id="heading${index}">
+//                                 <h2 class="mb-0">
+//                                 <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+//                                     aria-expanded="false" aria-controls="collapse${index}">
+//                                    <b>Breaking News ${index+1}:</b> ${element["title"]}
+//                                 </button>
+//                                 </h2>
+//                             </div>
+
+//                             <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
+//                                 <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
+//                             </div>
+//                         </div>`;
+//             newsHtml += news;
+//         });
+//         newsAccordion.innerHTML = newsHtml;
+//     } else {
+//         console.log("Some error occured")
+//     }
+// }
+
+// xhr.send()
